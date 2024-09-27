4f2cd2348ec0bb78cf05fd01727a6aff7a3b432f
Aleksei Eliseev <i@d5h.ru>
2024-09-27 16:47:42 +0300

    пробный коммит

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,107 @@
 package main
 
-func main() {}
+import (
+	"io"
+	"math/rand"
+	"net/http"
+)
+
+const form = `<html>
+    <head>
+    <title></title>
+    </head>
+    <body>
+        <form action="/" method="post" enctype="text/plain">
+            <label>Ссылка <input type="text" name="url"></label>
+            <button type="submit">yandex.ru</button>
+        </form>
+    </body>
+</html>`
+
+func randChar() int {
+	max := 122
+	min := 48
+	res := rand.Intn(max-min) + min
+	if res > 57 && res < 65 || res > 90 && res < 97 {
+		return randChar()
+	}
+	return res
+}
+
+func createUrl(url string) string {
+	sortUrl := []byte("http://localhost:8080/")
+	val, ok := outUrls[url]
+	if ok {
+		return val
+	}
+	for i := 0; i < 8; i++ {
+		sortUrl = append(sortUrl, byte(randChar()))
+	}
+	result := string(sortUrl)
+	_, ok = shortUrls[result]
+	if ok {
+		return createUrl(url)
+	}
+	outUrls[url] = result
+	shortUrls[result] = url
+	return result
+}
+
+func getUrl(url string) (bool, string) {
+	val, ok := shortUrls[url]
+	if ok {
+		return true, val
+	}
+	return false, ""
+}
+
+func checkHeader(hd http.Header, key string, val string) bool {
+	for k, v := range hd {
+		if key == k {
+			for _, vv := range v {
+				if vv == val {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
+func basePath(responce http.ResponseWriter, request *http.Request) {
+	url := request.URL.Path
+	if request.Method == http.MethodPost && url == "/" && checkHeader(request.Header, "Content-Type", "text/plain") {
+		responce.Header().Add("Content-Type", "text/plain")
+		responce.WriteHeader(http.StatusCreated)
+		request.ParseForm()
+		url, err := io.ReadAll(request.Body)
+		if err != nil {
+			panic(err)
+		}
+		body := createUrl(string(url))
+		responce.Write([]byte(body))
+		return
+	} else if request.Method == http.MethodGet {
+		ok, outUrl := getUrl("http://localhost:8080" + url)
+		if ok {
+			responce.Header().Add("Location", outUrl)
+			responce.WriteHeader(http.StatusTemporaryRedirect)
+			return
+		}
+	}
+	responce.WriteHeader(http.StatusBadRequest)
+	//responce.Write([]byte(form))
+}
+
+var outUrls = make(map[string]string)
+var shortUrls = make(map[string]string)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", basePath)
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/h1067675/shortUrl
+
+go 1.23.0
